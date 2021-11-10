7fcf59f318d0cedaf0dca933a3303af4b9c4a863
1337Miko1337 <kolyalevchenko18@gmail.com>
2021-11-10 22:53:07 +0200

    find maze exit

--- some program.py
+++ some program.py
@@ -14,27 +14,33 @@ rmax = len(matr)
 cmax = len(matr[0])
 
 
-def BFS(r, c):
+def BFS(r, c, r_finish, c_finish, k=1):
     que = []
-    matr[r][c] = 0
+    matr[r][c] = 2
     que.append([r, c])
     while len(que) > 0:
+        k += 1
+        a = k
         tmp = que.pop(0)
         r = tmp[0]
         c = tmp[1]
-        print('Координати: ', r, c)
-        if ((c + 1) < cmax) and (matr[r][c + 1] == 1):
-            que.append([r, c + 1])
-            matr[r][c + 1] = 0  # якщо можна йти вправо, то йдемо
-        if ((r + 1) < rmax) and (matr[r + 1][c] == 1):
-            que.append([r + 1, c])
-            matr[r + 1][c] = 0  # якщо можна йти вниз, то йдемо
-        if ((c - 1) >= 0) and (matr[r][c - 1] == 1):
-            que.append([r, c - 1])
-            matr[r][c - 1] = 0  # якщо можна йти вліво, то йдемо
-        if ((r - 1) >= 0) and (matr[r - 1][c] == 1):
-            que.append([r - 1, c])
-            matr[r - 1][c] = 0  # якщо можна йти вгору, то йдемо
+        while matr[r][c] != a:
+            a -= 1
+        if matr[r_finish][c_finish] == 1:
+            if ((c + 1) < cmax) and (matr[r][c + 1] == 1):
+                que.append([r, c + 1])
+                matr[r][c + 1] = a + 1
+            if ((r + 1) < rmax) and (matr[r + 1][c] == 1):
+                que.append([r + 1, c])
+                matr[r + 1][c] = a + 1
+            if ((c - 1) >= 0) and (matr[r][c - 1] == 1):
+                que.append([r, c - 1])
+                matr[r][c - 1] = a + 1
+            if ((r - 1) >= 0) and (matr[r - 1][c] == 1):
+                que.append([r - 1, c])
+                matr[r - 1][c] = a + 1
+    return a
 
 
-BFS(0, 1)
+a=BFS(1, 1, 9, 1)
+print(matr)
