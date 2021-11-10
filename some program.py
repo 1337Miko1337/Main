f2bee963cfb53ee372fe353e588a93468dce6a5f
1337Miko1337 <kolyalevchenko18@gmail.com>
2021-11-10 23:05:52 +0200

    add new feature road to exit maze

--- some program.py
+++ some program.py
@@ -9,7 +9,6 @@ matr = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
         [1, 1, 0, 0, 0, 0, 0, 0, 0, 1]]
 
-
 rmax = len(matr)
 cmax = len(matr[0])
 
@@ -42,5 +41,32 @@ def BFS(r, c, r_finish, c_finish, k=1):
     return a
 
 
-a=BFS(1, 1, 9, 1)
-print(matr)
+def road(r, c, k=2):
+    count = 0
+    a = []
+    while k > 2:
+        if ((c + 1) < cmax) and (matr[r][c + 1] == k - 1):
+            a.append([r, c + 1])
+            count += 1
+            c += 1
+            k -= 1
+        if ((r + 1) < rmax) and (matr[r + 1][c] == k - 1):
+            a.append([r + 1, c])
+            count += 1
+            r += 1
+            k -= 1
+        if ((c - 1) >= 0) and (matr[r][c - 1] == k - 1):
+            a.append([r, c - 1])
+            c = c - 1
+            k -= 1
+            count += 1
+        if ((r - 1) >= 0) and (matr[r - 1][c] == k - 1):
+            a.append([r - 1, c])
+            count += 1
+            r -= 1
+            k -= 1
+    return a
+
+b = BFS(1, 1, 9, 1)
+a = road(9, 1, b)
+print(a)
