bf836af55e497f219b84cbb397ade5882d6dcafe
1337Miko1337 <kolyalevchenko18@gmail.com>
2021-11-11 08:59:56 +0200

    fixed bug

--- some program.py
+++ some program.py
@@ -10,18 +10,16 @@ f.close()
 
 
 def DFS(r, c, r_finish, c_finish):
-    print( r, '-', c)
-    print(r != r_finish)
-    print(c != c_finish)
+    matr[r][c] = 0
     if (r != r_finish) or (c != c_finish):
         if ((c + 1) < cmax) and (matr[r][c + 1] == 1):
             DFS(r, c + 1, r_finish, c_finish)
         if ((r + 1) < rmax) and (matr[r + 1][c] == 1):
             DFS(r + 1, c, r_finish, c_finish)
         if ((c - 1) >= 0) and (matr[r][c - 1] == 1):
-            DFS(r, c - 1, c_finish)
+            DFS(r, c - 1, r_finish, c_finish)
         if ((r - 1) >= 0) and (matr[r - 1][c] == 1):
-            DFS(r - 1, c, r_finish)
+            DFS(r - 1, c, r_finish, c_finish)
     elif (r == r_finish) and (c == c_finish):
         exit()
         return
